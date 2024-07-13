970e47df1213bf5f26a24ae07b5313d952e0e0c7
Polycarp <polycarpwathuta39@gmail.com>
2024-07-14 01:31:52 +0300

    refactor: Moved the products routes callback functions to their own folder called controllers

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,28 +1,12 @@
 import { Router } from "express";
-import productModel from "../models/product.model.js";
+import { deleteProduct, getProduct, getProducts, patchProduct, postProduct } from "../controllers/products.js";
 
 const productsRouter = Router();
 
-productsRouter.get('/', (req, res) => {
-    res.json({mssg: "Product found"});
-});
-
-productsRouter.post('/', async (req, res) => {
-    try {
-        const { image, category, description, price, quantity } = req.body;
-        const product = await productModel.create({ image, category, description, price, quantity });
-        res.status(201).json(product);
-    } catch (error) {
-        res.status(400).json({error: error.message});
-    }
-});
-
-productsRouter.put('/:id', (req, res) => {
-    res.json({mssg: "Product Updated"});
-});
-
-productsRouter.delete('/:id', (req, res) => {
-    res.json({mssg: "Product Deleted"});
-});
+productsRouter.get('/', getProducts);
+productsRouter.get('/:id', getProduct);
+productsRouter.post('/', postProduct);
+productsRouter.patch('/:id', patchProduct);
+productsRouter.delete('/:id', deleteProduct);
 
 export default productsRouter;
