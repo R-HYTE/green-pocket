a951f8ac91872f64fcef75f499655e10f5a58df8
Polycarp <polycarpwathuta39@gmail.com>
2024-07-14 01:31:16 +0300

    refactor: Moved the feedback routes callback functions to their own folder called controllers

--- backend/routes/feedbacks.js
+++ backend/routes/feedbacks.js
@@ -1,28 +1,12 @@
 import { Router } from "express";
-import FeedbackModel from "../models/feedback.model.js";
+import { deleteFeedback, getFeedback, getFeedbacks, patchFeedback, postFeedback } from "../controllers/feedback.js";
 
 const feedbacksRouter = Router();
 
-feedbacksRouter.get('/', (req, res) => {
-    res.json({mssg: "Feedback found"});
-});
-
-feedbacksRouter.post('/', async (req, res) => {
-    try {
-        const { comment, stars, image_url } = req.body;
-        const feedback = await FeedbackModel.create({comment, stars, image_url});
-        res.status(201).json(feedback);
-    } catch (error) {
-        res.status(400).json({error: error.message});
-    }   
-});
-
-feedbacksRouter.put('/:id', (req, res) => {
-    res.json({mssg: "Feedback Updated"});
-});
-
-feedbacksRouter.delete('/:id', (req, res) => {
-    res.json({mssg: "Feedback Deleted"});
-});
+feedbacksRouter.get('/', getFeedbacks);
+feedbacksRouter.get('/:id', getFeedback);
+feedbacksRouter.post('/', postFeedback);
+feedbacksRouter.patch('/:id', patchFeedback);
+feedbacksRouter.delete('/:id', deleteFeedback);
 
 export default feedbacksRouter;
