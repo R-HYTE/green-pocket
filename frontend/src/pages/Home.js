8b3b0fd3efbec82ebafbda6555624b1f64fa5610
Polycarp <polycarpwathuta39@gmail.com>
2024-07-17 01:00:07 +0300

    feat: Add fetch featute to retrieve from backend server

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,28 @@
+import { useState, useEffect } from "react";
+
 function Home() {
+    const [products, setProducts] = useState(null);
+
+    useEffect(() => {
+        const fetchProducts = async () => {
+            const response = await fetch('/api/products');
+            const json = await response.json();
+    
+            if (response.ok) {
+                setProducts(json);
+            }
+        };
+
+        fetchProducts();
+    }, []);
+
     return (
         <div className="home">
-            <h2>Welcome to the home page</h2>
+            <div className="products">
+                {products && products.map(product => (
+                    <p key={product._id}>{ product.description }</p>
+                ))}
+            </div>
         </div>
     )
 };
